e02ead313e72d91a786483f5ed749fa0981c5ec6
Michael Newman <coloradohokie@gmail.com>
2019-12-20 07:09:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,19 @@
+=begin
 def reverse_each_word(string)
   new_string = ""
   array = string.split(" ")
   array.each do |word|
+    new_string << word.reverse
+    new_string << " "
+  end #block
+  new_string.chop
+end #method
+=end
+
+def reverse_each_word(string)
+  new_string = ""
+  array = string.split(" ")
+  array.collect do |word|
     new_string << word.reverse
     new_string << " "
   end #block
